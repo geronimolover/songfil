55510d0f4f17c1e69500d13e2365a66bb4723de9
Geronimo Lover <84311253+geronimolover@users.noreply.github.com>
2023-05-15 14:38:40 +0530

    Update song.py

--- FilterBot/song.py
+++ FilterBot/song.py
@@ -1,54 +1,74 @@
 import os
-import logging
+import requests
+import time
 import youtube_dl
+from youtube_search import YoutubeSearch
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-# enable logging
-logging.basicConfig(level=logging.DEBUG)
+app = Client("my_bot")
 
-app = Client("download_bot")
-
-
-def download_audio(url):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '320',
-        }],
-        'outtmpl': '%(id)s.%(ext)s'
-    }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        try:
-            info_dict = ydl.extract_info(url, download=True)
-            filename = ydl.prepare_filename(info_dict)
-        except Exception as e:
-            raise e
-
-    return filename
-
-
-
-@app.on_message(filters.command("m"))
-def text_handler(client, message: Message):
+@app.on_message(filters.command(['s']))
+def a(client, message: Message):
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    m = message.reply('🔎 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠 𝐭𝐡𝐞 𝐬𝐨𝐧𝐠...')
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
-        # search for the song on youtube and get video url
-        query = " ".join(message.text.split()[1:])
-        with youtube_dl.YoutubeDL() as ydl:
-            search_results = ydl.extract_info(f"ytsearch:{query}", download=False)['entries']
-            video_url = search_results[0]['webpage_url']
+        results = []
+        count = 0
+        while len(results) == 0 and count < 6:
+            if count > 0:
+                time.sleep(1)
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            count += 1
+        # results = YoutubeSearch(query, max_results=1).to_dict()
+        try:
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            duration = results[0]["duration"]
 
-        # download and send audio file to the user
-        filename = download_audio(video_url)
-        with open(filename, "rb") as f:
-            client.send_audio(message.chat.id, f, title=query)
+            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFERRED DURATION IN SECONDS AND EDIT THE MESSAGE TOO
+            # if time_to_seconds(duration) >= 1800:  # duration limit
+            #     m.edit("Exceeded 30mins cap")
+            #     return
 
-        # delete the downloaded audio file
-        os.remove(filename)
+            views = results[0]["views"]
+            thumb_name = f'thumb{message.message_id}.jpg'
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, 'wb').write(thumb.content)
 
+        except Exception as e:
+            print(e)
+            m.edit(f"𝐅𝐨𝐮𝐧𝐝 𝐍𝐨𝐭𝐡𝐢𝐧𝐠. {e}")
+            return
     except Exception as e:
-        message.reply(f"Sorry, I encountered an error while processing your request.\n{e}")
-        logging.exception(e)
+        m.edit("✖️ 𝐅𝐨𝐮𝐧𝐝 𝐍𝐨𝐭𝐡𝐢𝐧𝐠. 𝐒𝐨𝐫𝐫𝐲.")
+        print(str(e))
+        return
+
+    m.edit("🔎 𝐅𝐢𝐧𝐝𝐢𝐧𝐠 𝐀 𝐒𝐨𝐧𝐠 🎶 𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭 ⏳ 𝐅𝐨𝐫 𝐅𝐞𝐰 𝐒𝐞𝐜𝐨𝐧𝐝𝐬 [🚀](https://telegra.ph/file/60b0489093120e762861f.mp4)")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f'🎧 𝐓𝐢𝐭𝐥𝐞 : [{title[:35]}]({link})\n⏳ 𝐃𝐮𝐫𝐚𝐭𝐢𝐨𝐧 : `{duration}`\n🎬 𝐒𝐨𝐮𝐫𝐜𝐞 : [Youtube](https://youtu.be/3pN0W4KzzNY)\n👁‍🗨 𝐕𝐢𝐞𝐰𝐬 : `{views}`\n\n💌 A Bot 𝐁𝐲 : @Animemusicarchive6'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        message.reply_audio(audio_file, caption=rep, parse_mode='md', quote=False, title=title, duration=dur, thumb=thumb_name)
+        m.delete()
+    except Exception as e:
+        m.edit("❌ 𝐄𝐫𝐫𝐨𝐫.")
+        print(e)
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
